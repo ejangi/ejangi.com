97ccdb55a40b22adfef241ecfdb7a88eb17bec8f
ejangi <james@ejangi.com>
2018-11-03 15:19:19 +1000

    An attempt to make the navigation more reliable when people reload the page.

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -57,49 +57,21 @@ const Site =( ( $, CachedArticle ) => {
     $( document ).ready( function() {
 
         window.baseLocation = window.location.pathname;
-    
-        if ( !$( 'body' ).hasClass( 'single' ) ) {
 
-            $( '<div id="single-container"><div class="close">&#x2715;</div><div class="content"></div></div>' ).appendTo( $( 'body' ) ).hide();
+        $( '<div id="single-container"><div class="close">&#x2715;</div><div class="content"></div></div>' ).appendTo( $( 'body' ) ).hide();
 
-            $( '#single-container .close' ).on( 'click', function () {
-                $( '#single-container' ).slideUp();
-            } );
+        $( '#single-container .close' ).on( 'click', function () {
+            $( '#single-container' ).slideUp();
+        } );
 
-            $( '.main-container > article.post.format-standard' ).each( function () {
+        $( '.main-container > article.post.format-standard' ).each( function () {
 
-                $( '<div class="shade"></div>' ).appendTo( $( this ) ).hide();
+            $( '<div class="shade"></div>' ).appendTo( $( this ) ).hide();
 
-                $( this ).on( 'click', function ( e ) {
-
-                    var url = '',
-                        date = '',
-                        that = $( this ),
-                        up = 0,
-                        maxUp = 10;
-
-                    if ( $( this ).not( 'article' ) ) {
-
-                        while ( $( that ).not( 'article' ) && up > maxUp ) {
-
-                            that = $( that ).parent();
-                            up = up + 1;
-
-                        }
-
-                    }
-
-                    url = $( that ).attr( 'data-permalink' );
-                    date = $( that ).attr( 'data-last-modified' );
-                    displayPost( url, date, that );
-
-                } );
-
-            } );
-
-            $( '.main-container > article.post > header a:link' ).each( function () {
+            $( this ).on( 'click', function ( e ) {
 
                 var url = '',
+                    date = '',
                     that = $( this ),
                     up = 0,
                     maxUp = 10;
@@ -115,65 +87,89 @@ const Site =( ( $, CachedArticle ) => {
 
                 }
 
-                $( this ).on( 'click', function( e ) {
-                    e.preventDefault();
-                    $( that ).trigger( 'click' );
-                } );
+                url = $( that ).attr( 'data-permalink' );
+                date = $( that ).attr( 'data-last-modified' );
+                displayPost( url, date, that );
 
             } );
 
-            $( '#single-container > .close' ).on( 'click', function( e ) {
+        } );
 
-                if ( window.history !== undefined ) {
-                    history.pushState( {}, "", window.baseLocation );    
+        $( '.main-container > article.post > header a:link' ).each( function () {
+
+            var url = '',
+                that = $( this ),
+                up = 0,
+                maxUp = 10;
+
+            if ( $( this ).not( 'article' ) ) {
+
+                while ( $( that ).not( 'article' ) && up > maxUp ) {
+
+                    that = $( that ).parent();
+                    up = up + 1;
+
                 }
 
-                var postId = $( '#single-container article' ).attr( 'id' );
+            }
 
-                $( 'body' ).css( 'position', 'static' ).animate({ scrollTop: ( $( '.main-container article#' + postId ).offset().top - 50 ) }, 300);
-
+            $( this ).on( 'click', function( e ) {
+                e.preventDefault();
+                $( that ).trigger( 'click' );
             } );
 
-            $( document ).keyup( function( e ) {
-                if ( e.which == 27 && $( '#single-container' ).is( ':visible' ) ) {
-                    $( '#single-container > .close' ).trigger( 'click' );
+        } );
+
+        $( '#single-container > .close' ).on( 'click', function( e ) {
+
+            if ( window.history !== undefined ) {
+                history.pushState( {}, "", window.baseLocation );    
+            }
+
+            var postId = $( '#single-container article' ).attr( 'id' );
+
+            $( 'body' ).css( 'position', 'static' ).animate({ scrollTop: ( $( '.main-container article#' + postId ).offset().top - 50 ) }, 300);
+
+        } );
+
+        $( document ).keyup( function( e ) {
+            if ( e.which == 27 && $( '#single-container' ).is( ':visible' ) ) {
+                $( '#single-container > .close' ).trigger( 'click' );
+            }
+        } );
+
+        $( '.main-nav a, .me-nav a' ).each( function () {
+
+            var href = $( this ).attr( 'href' ),
+                url = document.createElement( 'a' );
+                url.href = href;
+
+            if ( url.origin !=  window.location.origin ) {
+                $( this ).attr( 'target', '_blank' );
+                return;
+            }
+
+            $( this ).on( 'click', function ( e ) {
+
+                e.preventDefault();
+
+                var url = $( this ).attr( 'href' ),
+                    date = $( this ).attr( 'data-last-modified' );
+
+                displayPost( url, date, this );
+
+                if ( $( '.me-nav' ).is( ':visible' ) ) {
+                    $( '.me-nav' ).slideUp();
                 }
-            } );
 
-            $( '.main-nav a, .me-nav a' ).each( function () {
-
-                var href = $( this ).attr( 'href' ),
-                    url = document.createElement( 'a' );
-                    url.href = href;
-
-                if ( url.origin !=  window.location.origin ) {
-                    $( this ).attr( 'target', '_blank' );
-                    return;
+                if ( $( '.main-nav' ).is( ':visible' ) ) {
+                    $( '.main-nav' ).slideUp();
+                    $( '.navbar-toggler' ).addClass( 'collapsed' );
                 }
 
-                $( this ).on( 'click', function ( e ) {
-
-                    e.preventDefault();
-
-                    var url = $( this ).attr( 'href' ),
-                        date = $( this ).attr( 'data-last-modified' );
-
-                    displayPost( url, date, this );
-
-                    if ( $( '.me-nav' ).is( ':visible' ) ) {
-                        $( '.me-nav' ).slideUp();
-                    }
-
-                    if ( $( '.main-nav' ).is( ':visible' ) ) {
-                        $( '.main-nav' ).slideUp();
-                        $( '.navbar-toggler' ).addClass( 'collapsed' );
-                    }
-
-                } );
-
             } );
 
-        }
+        } );
 
     });
 
